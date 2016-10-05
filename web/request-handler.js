453ce07eab14bc62dc1c986c2a5086adaf695510
Cheney Mao <cheneymao94@gmail.com>
2016-10-05 11:56:32 -0700

    Route / or /index.html to return contents of index.html

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,16 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  if (req.url.match(/^\/$|\/index.html/)) {
+    fs.readFile(archive.paths.siteAssets + '/index.html', function(err, data) {
+      res.end(data);
+    });    
+  } else if (req.url === '/styles.css') {
+    fs.readFile(archive.paths.siteAssets + '/styles.css', function(err, data) {
+      res.end(data);
+    }); 
+  }
 };
