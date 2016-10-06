6823ac09b5e8af6dcc790358311d4d20af34225a
kbchun <kbchun5712@gmail.com>
2016-10-05 19:31:11 -0700

    Prevent duplicates in sites.txt

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,6 +1,11 @@
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
 var archive = require('../helpers/archive-helpers');
+var _ = require('underscore');
 
-
-
+archive.readListOfUrls(function(listUrls) {
+  archive.readArchiveOfUrls(function(archiveUrls) {
+    // console.log(_.difference(listUrls, archiveUrls));
+    archive.downloadUrls(_.difference(listUrls, archiveUrls));
+  });
+});
