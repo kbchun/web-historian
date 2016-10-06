d9d6998868ba5e058264d5ac7444768d65207ad1
kbchun <kbchun5712@gmail.com>
2016-10-05 17:38:20 -0700

    Refactor code to handle GET and POST requests

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,62 +1,75 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
 var fs = require('fs');
-var httpHelpers = require('http-helpers');
+var httpHelpers = require('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  // redirect to home page
-  if (req.url.match(/^\/$|\/index\.html/)) {
-    fs.readFile(archive.paths.siteAssets + '/index.html', 'utf8', function(err, data) {
-      res.end(data);
-    });
+  var slicedURL = req.url.slice(1);
 
-  // import css file
-  } else if (req.url === '/styles.css') {
-    fs.readFile(archive.paths.siteAssets + '/styles.css', 'utf8', function(err, data) {
-      res.end(data);
-    }); 
+  if (req.method === 'GET') {
+    // redirect to home page
+    if (req.url.match(/^\/$|\/index\.html/)) {
+      fs.readFile(archive.paths.siteAssets + '/index.html', 'utf8', function(err, data) {
+        res.writeHead(200, httpHelpers.headers);
+        res.end(data);
+      });
 
-  // check if url starts with www. and ends with .com
-  } else if (req.url.match(/^\/www\..*\.com$/)) {
-    var slicedURL = req.url.slice(1);
-    // check if url is in archive
-    archive.isUrlArchived(slicedURL, function(archived) {
-      // if url is in archive
-      if (archived) {
-        // serve webpage to client
-        fs.readFile(archive.paths.archivedSites + req.url, 'utf8', function(err, data) {
-          res.end(data);
-        }); 
+    // import css file
+    } else if (req.url === '/styles.css') {
+      fs.readFile(archive.paths.siteAssets + '/styles.css', 'utf8', function(err, data) {
+        res.end(data);
+      }); 
 
-      // if url is not in archive (post request)
-      } else {
-        // check if url is in list
-        archive.isUrlInList(slicedURL, function(listed) {
-          //if url is in list
-          if (listed) {
-            fs.readFile(archive.paths.siteAssets + '/loading.html', 'utf8', function(err, data) {
-              res.end(data);
-            });
-          //if url is not in list
-          } else {
-            // let client know page is loading
-            // add url to list
-            archive.addUrlToList(slicedURL, function() {
-              console.log(slicedURL + ' added to list');
-            });
+    // check if url starts with www. and ends with .com
+    } else if (req.url.match(/^\/www\..*\.com$/)) {
+      // check if url is in archive
+      archive.isUrlArchived(slicedURL, function(archived) {
+        // if url is in archive
+        if (archived) {
+          // serve webpage to client
+          res.writeHead(200, httpHelpers.headers);
+          fs.readFile(archive.paths.archivedSites + req.url, 'utf8', function(err, data) {
+            res.end(data);
+          });
 
-            // let client know page is loading
-            fs.readFile(archive.paths.siteAssets + '/loading.html', 'utf8', function(err, data) {
-              res.end(data);
-            });
-          }
-        });
-      }
+        // if url is not in archive (post request)
+        } else {
+          // check if url is in list
+          archive.isUrlInList(slicedURL, function(listed) {
+            //if url is in list
+            if (listed) {
+              res.writeHead(202, httpHelpers.headers);
+              fs.readFile(archive.paths.siteAssets + '/loading.html', 'utf8', function(err, data) {
+                res.end(data);
+              });
+            //if url is not in list
+            } else {
+              // let client know page is loading
+              // make special interface to tell user to use our textbox
+              res.writeHead(404, httpHelpers.headers);
+              res.end();
+            }
+          });
+        }
+      });
+    } else {
+      res.writeHead(404, httpHelpers.headers);
+      res.end();
+    }
+
+  } else if (req.method === 'POST') {
+    //if url is not in list
+    // add url to list
+    // POST request
+    res.writeHead(201, httpHelpers.headers);
+    archive.addUrlToList(slicedURL, function() {
+      console.log(req.url + 'added to list');
+      console.log(slicedURL + ' added to list');
     });
   }
 
-
+  console.log('serving request type ', req.method, ' at localhost');
 
 
   // if (req.url === '/www.google.com') {
