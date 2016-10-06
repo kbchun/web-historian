53b43e9789f10cb8e150ec1d06ceb79519a0e72e
kbchun <kbchun5712@gmail.com>
2016-10-05 20:24:36 -0700

    Implement htmlfetcher and created cron text file

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -3,9 +3,12 @@
 var archive = require('../helpers/archive-helpers');
 var _ = require('underscore');
 
+// archive.downloadUrls(['www.google.com']);
 archive.readListOfUrls(function(listUrls) {
   archive.readArchiveOfUrls(function(archiveUrls) {
-    // console.log(_.difference(listUrls, archiveUrls));
+     // console.log(_.difference(listUrls, archiveUrls));
+    // var x = _.difference(listUrls, archiveUrls);
+    console.log(_.difference(listUrls, archiveUrls));
     archive.downloadUrls(_.difference(listUrls, archiveUrls));
   });
 });
